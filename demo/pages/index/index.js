4aadc5a60a70f9e5db71ad121e5cbb54b4b4adf2
ccw <ccw1078@163.com>
2019-12-27 17:50:28 +0800

    change block data structure

--- demo/pages/index/index.js
+++ demo/pages/index/index.js
@@ -6,16 +6,18 @@ Page({
    */
   data: {
     blocks: [
-      { name: 'a', speed: 2, start: 0, end: 100 },
-      { name: 'b', speed: 4, start: 0, end: 100 },
-      { name: 'c', speed: 6, start: 0, end: 100 },
-      { name: 'd', speed: 8, start: 0, end: 100 },
-      { name: 'e', speed: 10, start: 0, end: 100 }
+      { name: 'a', estimated: 30000, start: 0},
+      { name: 'b', estimated: 40000, start: 0 },
+      { name: 'c', estimated: 60000, start: 0 },
+      { name: 'd', estimated: 80000, start: 0 },
+      { name: 'e', estimated: 100000, start: 0 }
     ]
   },
 
   onLoad: function (options) {
-    var that = this;
+    let that = this;
+    init_blocks(that);
+    console.log(Date.now());
     wx.createSelectorQuery().select('.ring').boundingClientRect(rect => {
       console.log(rect);
       that.canvas_width = parseInt(rect.width / 2);
@@ -32,24 +34,23 @@ Page({
 function update_rings(that) {
   that.rings.forEach((item, index) => {
     let block = that.data.blocks[index];
-    let current = item.current ? item.current : block.start;
+    let current = Date.now() - block.start;
     let x = that.canvas_width, y = that.canvas_height;
     let r = x - 5, start = -0.5 * Math.PI;
     item.setTextAlign("center");
     item.setTextBaseline("middle");
     item.setFontSize(16);
-    if (current < block.end) {
-      let percent = current / block.end;
+    if (current < block.estimated) {
+      let percent = current / block.estimated;
       let num = (percent * 2 * Math.PI) + start;
       item.arc(x, y, r, start, num);
-      item.current = current + block.speed;
       item.setStrokeStyle("#f76260");
       item.setLineWidth("10");
       item.setLineCap("butt");
       item.stroke();
       item.beginPath();
       item.setFillStyle("#f76260");
-      item.fillText(current + "%", x, y);
+      item.fillText(parseInt(percent*100) + "%", x, y);
     } else {
       item.setFillStyle("#2BA245");
       item.fillText("100%", x, y);
@@ -62,5 +63,16 @@ function update_rings(that) {
       update_rings(that)
     }
   }, 1000);
+}
+
+
+function init_blocks(that){
+  let blocks = that.data.blocks;
+  blocks.forEach(item => {
+    item.start = Date.now();
+  });
+  that.setData({
+    blocks: blocks
+  });
 }
 
